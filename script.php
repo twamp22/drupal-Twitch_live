a462df187da00898b9b6c2da05cb071f18ad9de0
twamp22 <drwright420@hotmail.com>
2015-08-08 09:58:01 -0700

    Fixed Header

--- script.php
+++ script.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * This is some PHP for Drupal that I have written to embed one(randomly) of the user's twitch streams.
- * If no Drupal users are live on Twitch, show one of the top Twitch channels.
  *
  * This script gets all available Twitch account channels from HybridAuth using Drupal's user entity.
  * If any of those channels are live, it will randomly select one to embed.
